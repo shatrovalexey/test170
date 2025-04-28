662705747c1acbbf5a179d14537afc9a491b0e40
Алексей <mail@ashatrov.ru>
2025-04-28 17:50:54 +0300

    Initial

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 
 if len(Sys.argv) < 2: raise Exception('Usage: ./run.py <skuID>')
 
-sku_id = Sys.argv[1]
+sku_id = int(Sys.argv[1])
 url_pattern = 'https://www.ozon.ru/api/entrypoint-api.bx/page/json/v2?url=%%2Fproduct%%2F%d%%2F%%3Flayout_container%%3Dreviewshelfpaginator&__rr=1&abt_att=1'
 rx_match = Regexp.compile('^webListReviews-[0-9]+-reviewshelfpaginator-[0-9]+$')
 
@@ -17,7 +17,8 @@ rx_match = Regexp.compile('^webListReviews-[0-9]+-reviewshelfpaginator-[0-9]+$')
 """
 def get_page_data(sku_id):
 	with SB(test=False, uc=True, headless2=True, undetectable=True) as sb:
-		sb.open(url_pattern % (int(sku_id)))
+		url = url_pattern % sku_id
+		sb.open(url)
 
 		return sb.find_element(By.CSS_SELECTOR, 'pre').get_attribute('textContent')
 
@@ -39,7 +40,4 @@ def get_comments(sku_id):
 """
 Тестирование
 """
-comments = get_comments(sku_id)
-
-with open('out.txt', 'w') as fh:
-	Json.dump(comments, fh)
+Json.dump(get_comments(sku_id), Sys.stdout)
